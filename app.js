781c211c7e1ce52e08066b132735315dccdf661a
Muhammad Ejaz <mejaz4349@gmail.com>
2022-06-18 19:40:50 +0500

    improve UI and other stuff

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var btn = document.getElementById('addbtn').addEventListener('click', addTask);
 let data = [];
+let taskmenu = false;
+let taskmenu1 = document.querySelector('.add-items');
+let taskbtn1 = document.querySelector('.addtaskbtn');
+let complete = document.getElementById('complete');
+
+taskbtn1.addEventListener('click', showHide);
+
+function showHide(){
+    if (taskmenu == false){
+        taskmenu1.style.transform = "translateY(0%)";
+        taskmenu = true;
+        taskbtn1.innerHTML = "Close"
+    }else{
+        taskmenu1.style.transform = "translateY(-100%)";
+        taskbtn1.innerHTML = "Add New Task"
+
+        taskmenu = false
+
+    }
+}
+
+
 
 
 function addTask() {
@@ -16,16 +38,14 @@ function addTask() {
         dataPush();
         task.innerHTML =
             `<div id="tasklist" class="list-wrapper">
-            <div class="list-wrapper">
-                <div>
-                    <h4>${title.value}</h4>
+            <div class="list-wrapper"><div><h4 id="complete">${title.value}</h4>
                     <ul class="d-flex flex-column-reverse todo-list">
                         <li> ${description.value} </li>
                     </ul>
                     <p>${date.value} </p>
                 </div>
                 <div class="taskbtn">
-                    <button onclick="editFn(this)" class="edit"> Edit</button>
+                    <button onclick="completetask(this)" class="edit"> Comppleted</button>
                     <button onclick="deleteFn(this)" class="delete"> Delete</button>
                 </div>
             </div>
@@ -52,3 +72,8 @@ function deleteFn(e){
  e.parentElement.parentElement.remove();
 }
 
+function completetask(e){
+    let completetask = e.parentElement.parentElement.firstChild.firstChild;
+    completetask.style.textDecoration = "line-through";
+    completetask.style.color = "gray";
+    }
