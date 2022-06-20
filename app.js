7735bc388ca425d1d5da8e15de5260edf2a76039
Muhammad Ejaz <98952856+MuhammadEjaz11@users.noreply.github.com>
2022-06-20 16:27:40 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,65 @@
 var btn = document.getElementById('addbtn').addEventListener('click', addTask);
 let data = [];
+let taskmenu = false;
+let taskmenu1 = document.querySelector('.add-items');
+let tasklistdiv = document.getElementById('tasks');
+let taskbtn1 = document.querySelector('.addtaskbtn');
+let complete = document.getElementById('complete');
+const retData = JSON.parse(localStorage.getItem("data"));
+taskbtn1.addEventListener('click', showHide);
+let clears = document.getElementById('clearall').addEventListener('click', clearAll);
+
+
+function retDataf(){
+    if(retData !== null){
+        console.log(retData);
+        for (var i = 0; i < retData.length; i++) {
+            
+            let dName = retData[i].title;
+            var dDescription = retData[i].description;
+            var dDate = retData[i].date;
+            const task = document.createElement("div");
+            var cont = document.getElementById('tasks');
+            task.innerHTML =
+            `<div id="parentremove"><div id="tasklist" class="list-wrapper">
+            <div class="list-wrapper"><div><h4 id="complete">${dName}</h4>
+                    <ul class="d-flex flex-column-reverse todo-list">
+                        <li> ${dDescription} </li>
+                    </ul>
+                    <p>${dDate} </p>
+                </div>
+                <div class="taskbtn">
+                    <button onclick="completetask(this)" class="edit"> Mark as Complete</button>
+                    
+                </div>
+            </div>
+           </div>
+        </div>`
+        cont.appendChild(task);
+            
+        }
+    }else{
+        console.log("abc");
+    }
+}
+retDataf();
+
+function showHide(){
+    if (taskmenu == false){
+        taskmenu1.style.transform = "translateY(0%)";
+        taskmenu = true;
+        taskbtn1.innerHTML = "Close"
+        tasklistdiv.style.transform = "translateY(0%)"
+    }else{
+        taskmenu1.style.transform = "translateY(-100%)";
+        taskbtn1.innerHTML = "Add New Task"
+        tasklistdiv.style.transform = "translateY(-100%)"
+        taskmenu = false
+
+    }
+}
+
+
 
 
 function addTask() {
@@ -15,19 +75,17 @@ function addTask() {
     else {
         dataPush();
         task.innerHTML =
-            `<div id="tasklist" class="list-wrapper">
-            <div class="list-wrapper">
-                <div>
-                    <h4>${title.value}</h4>
+            `<div id="parentremove"><div id="tasklist" class="list-wrapper">
+            <div class="list-wrapper"><div><h4 id="complete">${title.value}</h4>
                     <ul class="d-flex flex-column-reverse todo-list">
                         <li> ${description.value} </li>
                     </ul>
                     <p>${date.value} </p>
                 </div>
                 <div class="taskbtn">
-                    <button onclick="editFn(this)" class="edit"> Edit</button>
-                    <button onclick="deleteFn(this)" class="delete"> Delete</button>
+                    <button onclick="completetask(this)" class="edit"> Mark as Complete</button>
                 </div>
+            </div>
             </div>
         </div>`
         cont.appendChild(task);
@@ -50,5 +108,19 @@ function dataPush(){
 
 function deleteFn(e){
  e.parentElement.parentElement.remove();
+
 }
 
+function completetask(e){
+    let comp = document.querySelector('.edit');
+    let completetask = e.parentElement.parentElement.firstChild.firstChild;
+    completetask.style.textDecoration = "line-through";
+    completetask.style.color = "gray";
+    comp.innerHTML = "Completed";
+
+}
+function clearAll(){
+    let tasks = document.getElementById('tasks');
+    tasks.innerHTML = "";
+    localStorage.clear();
+}
